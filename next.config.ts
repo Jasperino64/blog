e8ae040f9382b4bd0d93b034225b954b6c0de7bc
Jasper Chen <jasper.s.chen@gmail.com>
2026-02-16 17:50:28 -0500

    increase body size limit

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,11 @@ const nextConfig: NextConfig = {
     ],
   },
   cacheComponents: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb",
+    },
+  },
 };
 
 export default nextConfig;
