c88b3d44bbf7bf5c43252f32a589fc704e153e58
Jasper Chen <jasper.s.chen@gmail.com>
2026-02-18 19:23:33 -0500

    update main page title

--- app/(shared-layout)/page.tsx
+++ app/(shared-layout)/page.tsx
@@ -1,3 +1,21 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Jasper's Stuff",
+  description: "Stuff I made and share with the world.",
+  category: "Web development",
+  authors: [{ name: "Jasper Chen" }],
+  generator: "Next.js",
+  keywords: ["blog", "Jasper's Blog", "Jasper Chen"],
+  openGraph: {
+    title: "Jasper's Stuff",
+    description: "Stuff I made and share with the world.",
+    type: "website",
+    locale: "en_US",
+    siteName: "Jasper's Stuff",
+  },
+};
+
 export default function Home() {
   return <div>Home</div>;
 }
